810c2367687aad3f68359d50ee7bd716c14df082
javier <javier.san.mail@gmail.com>
2021-02-25 19:00:33 +0100

    TY objects, arrys and triples

--- 2-Starting-project/app.ts
+++ 2-Starting-project/app.ts
@@ -13,17 +13,12 @@
         hobbies: ['Sports', 'Cooking'],
         role: [2, 'author']
       };
-      
       // person.role.push('admin');
       // person.role[1] = 10;
-      
       // person.role = [0, 'admin', 'user'];
-      
       let favoriteActivities: string[];
       favoriteActivities = ['Sports'];
-      
       console.log(person.name);
-      
       for (const hobby of person.hobbies) {
         console.log(hobby.toUpperCase());
         // console.log(hobby.map()); // !!! ERROR !!!
